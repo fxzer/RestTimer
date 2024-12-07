99cbb7036b964b8134132e64ff1c22cbd6a8f3cc
fxzer <fxzer8888@163.com>
2024-12-07 11:01:13 +0800

    Initial Commit

---
+++ RestTimer/RestTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestTimerApp.swift
+//  RestTimer
+//
+//  Created by fxj on 2024/12/7.
+//
+
+import SwiftUI
+
+@main
+struct RestTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
